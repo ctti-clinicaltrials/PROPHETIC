3be440feda663ac98f50610f3ead8bb4ec9f62eb
casey choiniere <choiniere@mbp.local>
2018-05-30 13:28:22 -0700

    Add config/env vars

--- client/src/containers/Home.jsx
+++ client/src/containers/Home.jsx
@@ -1,24 +1,85 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { observer } from 'mobx-react';
 import { Col, Row } from 'react-grid-system';
 import AuthStore from '../stores/AuthStore'
 import MainStore from '../stores/MainStore'
 import Paper from '@material-ui/core/Paper'
+import { withStyles } from '@material-ui/core/styles';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+    const styles = theme => ({
+        root: {
+            width: '100%',
+        },
+        heading: {
+            fontSize: theme.typography.pxToRem(15),
+            fontWeight: theme.typography.fontWeightRegular,
+        },
+    });
+
 
 @observer
 class Home extends Component {
     componentDidMount() {
         MainStore.test();
         AuthStore.getProfile();
+        AuthStore.getDDSApiToken();
     }
 
     render() {
         let {userProfile} = AuthStore;
+        const { classes } = this.props;
+
         return (
-               <span>{userProfile && <img src={userProfile.picture} alt="profile" style={{maxWidth: 80}}/>}</span>
+            <span>{userProfile && <img src={userProfile.picture} alt="profile" style={{maxWidth: 80}}/>}
+                <div className={classes.root}>
+                    <ExpansionPanel>
+                        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                            <Typography className={classes.heading}>Expansion Panel 1</Typography>
+                        </ExpansionPanelSummary>
+                        <ExpansionPanelDetails>
+                            <Typography>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                                sit amet blandit leo lobortis eget.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                                sit amet blandit leo lobortis eget.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                                sit amet blandit leo lobortis eget.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                                sit amet blandit leo lobortis eget.
+                            </Typography>
+                            <div style={{float: 'right'}}><ExpandMoreIcon /></div>
+                        </ExpansionPanelDetails>
+                    </ExpansionPanel>
+                    <ExpansionPanel>
+                        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                            <Typography className={classes.heading}>Expansion Panel 2</Typography>
+                        </ExpansionPanelSummary>
+                        <ExpansionPanelDetails>
+                            <Typography>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                                sit amet blandit leo lobortis eget.
+                            </Typography>
+                        </ExpansionPanelDetails>
+                    </ExpansionPanel>
+                    <ExpansionPanel>
+                        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                            <Typography className={classes.heading}>Disabled Expansion Panel</Typography>
+                        </ExpansionPanelSummary>
+                    </ExpansionPanel>
+                </div>
+            </span>
         );
     }
 }
+
+Home.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Home);
 {/*<Row>*/}
     {/*{*/}
         {/*projects.map((p) => {*/}
@@ -45,4 +106,5 @@ class Home extends Component {
         {/*One of three columns*/}
     {/*</Col>*/}
 {/*</Row>*/}
-export default Home;
+// export default Home;
+
