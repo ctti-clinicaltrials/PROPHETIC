28483f952cbaf73ed43f177e1aca494b4fd4784e
casey choiniere <caseychoiniere@gmail.com>
2018-05-31 14:30:05 -0700

    Fix clientID in AuthStore

--- client/src/stores/AuthStore.js
+++ client/src/stores/AuthStore.js
@@ -10,7 +10,7 @@ export class AuthStore {
 
     constructor() {
         this.auth0 = new auth0.WebAuth({
-            clientID: config.CLIENT_ID || '',
+            clientID: config.CLIENT_ID,
             domain: config.AUTH0_URL,
             responseType: 'token id_token',
             audience: config.API_ID,
