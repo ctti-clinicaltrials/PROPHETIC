3e1fcf21d9ea3e34f07527f99ecb094f9d7f1b17
casey choiniere <caseychoiniere@gmail.com>
2018-06-28 15:45:21 -0700

    Update heroku config var

--- client/src/config.js
+++ client/src/config.js
@@ -17,7 +17,7 @@ clientID = clientID || '';
 
 const ddsApiUrl = !process.env.NODE_ENV ? env.REACT_APP_DDS_API_URL : process.env.REACT_APP_DDS_API_URL;
 
-const ddsProjectID = process.env.NODE_ENV !== 'production' ? env.REACT_APP_DDS_PROJECT_ID : process.env.REACT_APP_DDS_PROJECT_ID;
+const ddsProjectID = !process.env.NODE_ENV ? env.REACT_APP_DDS_PROJECT_ID : process.env.REACT_APP_DDS_PROJECT_ID;
 
 const jwksURI = !process.env.NODE_ENV ? env.REACT_APP_JWKS_URI : process.env.REACT_APP_JWKS_URI;
 
