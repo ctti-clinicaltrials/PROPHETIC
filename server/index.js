cec56cf34caf1b123785baeaf01d2d941e454c93
casey choiniere <caseychoiniere@gmail.com>
2018-07-09 16:18:13 -0700

    Add user survey POST endpoint

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const agentToken = require('./routes/agentToken');
+const userResponse = require('./routes/userResponse');
 const cors = require('cors');
 const corsOptions = { origin: '*' };
 const bodyParser = require('body-parser');
@@ -17,6 +18,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, '../client/build')), cors(corsOptions), helmet(), bodyParser.json());
@@ -24,10 +27,11 @@ app.use(express.static(path.resolve(__dirname, '../client/build')), cors(corsOpt
 // Connect to DB
 mongoose.connect(dbConfig.DB).then(
     () => {console.log('Database is connected') },
-    err => { console.log('Can not connect to the database' +err)
+    err => { console.log('Cannot connect to the database' +err)
 });
 
 app.use('/api/agent-token', agentToken);
+app.use('/api/user-response', userResponse);
 
 app.get('/api/status', jwt.check(), (req, res) => {
     res.set('Content-Type', 'application/json');
