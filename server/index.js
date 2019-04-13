a6a6ecb273f252a0a98e71f134ebd46c562d3b9f
casey choiniere <caseychoiniere@gmail.com>
2019-04-13 13:36:32 -0700

    Check DB Config vars

--- server/index.js
+++ server/index.js
@@ -25,6 +25,7 @@ app.use(
 );
 
 mongoose.connect(dbConfig.DB).then(
+    console.log(dbConfig.DB),
     () => {console.log('Database is connected') },
     err => { console.log('Cannot connect to the database' +err)
 });
